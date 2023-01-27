c7113323e570fb03146bb24438556f9a31cd3621
Biketova111 <89241781930@mail.ru>
2023-01-27 09:36:18 +0900

    add example005_ifelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя: ");
+string userName =Console.ReadLine();
+// if(userName.ToLower() == "елена")
+{
+    Console.WriteLine("Ура! Это Елена");
+}
+else
+{
+ Console.Write("Привет, ");
+ Console.WriteLine(userName); 
+}
