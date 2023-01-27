ec6aebe9bfa7d1966cb7dfc93ef330bf16fa23a1
Biketova111 <89241781930@mail.ru>
2023-01-27 09:43:42 +0900

    add2 example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA= 3;
-int numberB=5;
+﻿int numberA= new Random().Next(1,10); // 123..9
+Console.Write("A = ");
+Console.WriteLine(numberA);
+int numberB= new Random().Next(1,10); // 123..9
+Console.Write("B = ");
+Console.WriteLine(numberB);
 int result = numberA+numberB;
+Console.Write("A+B = ");
 Console.WriteLine(result);
